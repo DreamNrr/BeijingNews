9464e6fa3fe4546a0aa46380cec93a0686686bad
DreamNrr <wzh74371233@163.com>
2017-06-02 11:21:14 +0800

    欢迎页面+引导页面

--- app/src/main/java/com/example/wzh/beijingnews/WelcomeActivity.java
+++ app/src/main/java/com/example/wzh/beijingnews/WelcomeActivity.java
@@ -1,13 +1,58 @@
 package com.example.wzh.beijingnews;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+import com.example.wzh.beijingnews.activity.GuideActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    private RelativeLayout rl_root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
+        rl_root = (RelativeLayout)findViewById(R.id.rl_root);
+
+        AlphaAnimation aa = new AlphaAnimation(0,1);
+        aa.setDuration(2000);
+        aa.setFillAfter(true);
+
+        ScaleAnimation sa = new ScaleAnimation(0,1,0,1, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        sa.setDuration(2000);
+        sa.setFillAfter(true);
+
+        RotateAnimation ra = new RotateAnimation(0,360,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        ra.setDuration(2000);
+        ra.setFillAfter(true);
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(sa);
+        set.addAnimation(ra);
+        set.addAnimation(aa);
+
+
+        rl_root.startAnimation(set);
+
+        set.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                Intent intent = new Intent(WelcomeActivity.this,GuideActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
     }
 }
